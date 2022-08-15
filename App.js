27f80334ad1f58259969fd41f340a9f8d1f6de5e
Smit-Kadawala <kadawalasmit@gmail.com>
2022-08-15 22:02:04 +0530

    pushNotification

---
+++ App.js
@@ -0,0 +1,36 @@
+//import liraries
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import ForegroundHandler from './src/helper/ForegroundHandler';
+import {
+  requestUserPermission,
+  notificationListener,
+} from './src/helper/notificationServices';
+
+// create a component
+const App = () => {
+  useEffect(() => {
+    requestUserPermission();
+    notificationListener();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <ForegroundHandler />
+      <Text>App</Text>
+    </View>
+  );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2c3e50',
+  },
+});
+
+//make this component available to the app
+export default App;
